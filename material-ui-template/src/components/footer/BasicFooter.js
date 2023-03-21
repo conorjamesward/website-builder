3a5eb359d7d67a43e7a29788ac6b651b4c9850a5
Conor Ward <sirconorjamesward@gmail.com>
2023-03-21 06:03:48 -0600

    contact data in footer

--- material-ui-template/src/components/footer/BasicFooter.js
+++ material-ui-template/src/components/footer/BasicFooter.js
@@ -8,15 +8,43 @@ export default function BasicFooter(props){
 
   return(
     <footer>
-      <div id="mainFooter">
+      <div className={styles.footerMain}>
         <Container>
-
+          {contact.email &&
+            <a rel='noreferrer' target="_blank" href={`mailto:${contact.email.address}`}>
+              {contact.email.icon}
+              <p>{contact.email.address}</p>
+            </a>
+          }
+          {contact.phone &&
+            <a rel='noreferrer' target="_blank" href={`tel:${contact.phone.number}`}>
+              {contact.phone.icon}
+              <p>{contact.phone.number}</p>
+            </a>
+          }
+          {contact.location &&
+          <a rel='noreferrer' target="_blank" href={contact.location.url}>
+            {contact.location.icon}
+            <p>{contact.location.address}</p>
+          </a>
+          }
+          {contact.hours &&
+            <span>
+              {contact.hours.icon}
+              <p>{contact.hours.hours}</p>
+            </span>
+          }
         </Container>
         <Container>
 
         </Container>
       </div>
       <div className={styles.credit}>
+        {social.length > 0 &&
+          <div className={styles.social}>
+            {social.map(social =><a href={social.url}>{social.icon}</a>)}
+          </div>
+        }
         {signature.signatureURL !== null &&
           <Link underline='hover' href={signature.signatureURL}>{signature.signature}</Link>
         }
--- material-ui-template/src/content/footer.js
+++ material-ui-template/src/content/footer.js
@@ -1,16 +1,42 @@
 import FacebookIcon from '@mui/icons-material/Facebook';
+import GitHubIcon from '@mui/icons-material/GitHub';
+
+import LocalPhoneIcon from '@mui/icons-material/LocalPhone';
+import EmailIcon from '@mui/icons-material/Email';
+import MapIcon from '@mui/icons-material/Map';
+import AccessTimeFilledIcon from '@mui/icons-material/AccessTimeFilled';
 
 const date = new Date()
 
 const footer = {
   social:[
     {
-      icon:<FacebookIcon/>,
+      icon:<FacebookIcon fontSize='large'/>,
+      url:''
+    },
+    {
+      icon:<GitHubIcon fontSize='large'/>,
       url:''
     },
   ],
   contact:{
-
+    email:{
+      address:'email@email.com',
+      icon:<EmailIcon fontSize='large'/>,
+    },
+    phone:{
+      number:'555-555-5555',
+      icon:<LocalPhoneIcon fontSize='large'/>,
+    },
+    location:{
+      address:'1600 Pennsylvania Avenue NW, Washington, DC 20500',
+      url:'https://www.google.com/maps/place/1600+Pennsylvania+Avenue+NW,+Washington,+DC+20500/@38.8975383,-77.0409888,17z/data=!3m1!4b1!4m6!3m5!1s0x89b7b78e00410e73:0xac8125363999a8f!8m2!3d38.8975342!4d-77.0365041!16s%2Fg%2F11c2hwc1d2?authuser=0',
+      icon:<MapIcon fontSize='large'/>,
+    },
+    hours:{
+      hours:'9-5 Monday Through Friday',
+      icon:<AccessTimeFilledIcon fontSize='large'/>,
+    }
   },
   signature:{
     signature:'Site built by Conor',
