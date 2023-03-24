7a25ebd4203748f022c4a0ce55f5693ed9d289fb
Conor Ward <sirconorjamesward@gmail.com>
2023-03-24 06:18:06 -0600

    footer works much better

--- material-ui-template/src/components/footer/BasicFooter.js
+++ material-ui-template/src/components/footer/BasicFooter.js
@@ -1,58 +1,77 @@
-import Container from '@mui/material/Container';
 import Link from '@mui/material/Link';
 import styles from '@/styles/Footer.module.css'
+import pageData from '@/content/pageData';
+import Typeograpghy from '@mui/material/Typography';
+import useMediaQuery from '@mui/material/useMediaQuery';
+
+function SocialSignature ({signature, social}){
+  return(
+    <div className={styles.credit}>
+      {social.length > 0 &&
+        <div className={styles.social}>
+          {social.map(social =><a key={social.title} href={social.url}>{social.icon}</a>)}
+        </div>
+      }
+      {signature.signatureURL !== null &&
+        <Link underline='hover' href={signature.signatureURL}>{signature.signature}</Link>
+      }
+      {signature.signatureURL === null &&
+        <p>{signature.signature}</p>
+      }
+      <p className={styles.copy}>&copy; {signature.copyright}</p>
+    </div>
+  )
+}
 
 export default function BasicFooter(props){
 
   const {social, contact, signature} = props.footerData
+  const pages = Object.keys(pageData)
+
+  const small = useMediaQuery('(max-width:480px)');
 
   return(
     <footer>
       <div className={styles.footerMain}>
-        <Container>
+        <div className={styles.siteMap}>
+          <Typeograpghy variant='h6'>Site Map</Typeograpghy>
+          {
+            pages.map(page => <Link underline='hover' key={`footer-${pageData[page].headTitle}`} href={pageData[page].url}>{pageData[page].buttonTitle}</Link>)
+          }
+        </div>
+        {!small &&
+          <SocialSignature signature={signature} social={social}/>
+        }
+        <div className={styles.contactContainer}>
           {contact.email &&
-            <a rel='noreferrer' target="_blank" href={`mailto:${contact.email.address}`}>
+            <a className={styles.contactLink} rel='noreferrer' target="_blank" href={`mailto:${contact.email.address}`}>
               {contact.email.icon}
-              <p>{contact.email.address}</p>
+              <p className={styles.contactInfo}>{contact.email.address}</p>
             </a>
           }
           {contact.phone &&
-            <a rel='noreferrer' target="_blank" href={`tel:${contact.phone.number}`}>
+            <a className={styles.contactLink} rel='noreferrer' target="_blank" href={`tel:${contact.phone.number}`}>
               {contact.phone.icon}
-              <p>{contact.phone.number}</p>
+              <p className={styles.contactInfo}>{contact.phone.number}</p>
             </a>
           }
           {contact.location &&
-          <a rel='noreferrer' target="_blank" href={contact.location.url}>
+          <a className={styles.contactLink} rel='noreferrer' target="_blank" href={contact.location.url}>
             {contact.location.icon}
-            <p>{contact.location.address}</p>
+            <p className={styles.contactInfo}>{contact.location.address}</p>
           </a>
           }
           {contact.hours &&
-            <span>
+            <span className={styles.contactLink}>
               {contact.hours.icon}
-              <p>{contact.hours.hours}</p>
+              <p className={styles.contactInfo}>{contact.hours.hours}</p>
             </span>
           }
-        </Container>
-        <Container>
-
-        </Container>
+        </div>
       </div>
-      <div className={styles.credit}>
-        {social.length > 0 &&
-          <div className={styles.social}>
-            {social.map(social =><a href={social.url}>{social.icon}</a>)}
-          </div>
-        }
-        {signature.signatureURL !== null &&
-          <Link underline='hover' href={signature.signatureURL}>{signature.signature}</Link>
-        }
-        {signature.signatureURL === null &&
-          <p>{signature.signature}</p>
-        }
-        <p className={styles.copy}>&copy; {signature.copyright}</p>
-      </div>
+      {small &&
+        <SocialSignature signature={signature} social={social}/>
+      }
     </footer>
   );
 }
--- material-ui-template/src/content/footer.js
+++ material-ui-template/src/content/footer.js
@@ -8,34 +8,40 @@ import AccessTimeFilledIcon from '@mui/icons-material/AccessTimeFilled';
 
 const date = new Date()
 
+const iconColor = 'primary'
+const socialIconsSize = 'large'
+const contactIconsSize = 'small'
+
 const footer = {
   social:[
     {
-      icon:<FacebookIcon fontSize='large'/>,
+      title:'Facebook',
+      icon:<FacebookIcon fontSize={socialIconsSize} color={iconColor}/>,
       url:''
     },
     {
-      icon:<GitHubIcon fontSize='large'/>,
+      title:'GitHub',
+      icon:<GitHubIcon fontSize={socialIconsSize} color={iconColor}/>,
       url:''
     },
   ],
   contact:{
     email:{
       address:'email@email.com',
-      icon:<EmailIcon fontSize='large'/>,
+      icon:<EmailIcon fontSize={contactIconsSize} color={iconColor}/>,
     },
     phone:{
       number:'555-555-5555',
-      icon:<LocalPhoneIcon fontSize='large'/>,
+      icon:<LocalPhoneIcon fontSize={contactIconsSize} color={iconColor}/>,
     },
     location:{
-      address:'1600 Pennsylvania Avenue NW, Washington, DC 20500',
-      url:'https://www.google.com/maps/place/1600+Pennsylvania+Avenue+NW,+Washington,+DC+20500/@38.8975383,-77.0409888,17z/data=!3m1!4b1!4m6!3m5!1s0x89b7b78e00410e73:0xac8125363999a8f!8m2!3d38.8975342!4d-77.0365041!16s%2Fg%2F11c2hwc1d2?authuser=0',
-      icon:<MapIcon fontSize='large'/>,
+      address:'1600 Pennsylvania Avenue NW',
+      url:'https://www.google.com/maps/place/The+White+House/@38.8987691,-77.0372802,17z/data=!3m1!5s0x89b7b7bce13fc169:0xb825fceceddb4d3c!4m15!1m8!3m7!1s0x89b7b7bd0e41c31b:0x5661d3aa16e785d4!2sPennsylvania+Avenue+NW,+Washington,+DC!3b1!8m2!3d38.8987649!4d-77.0350915!16s%2Fg%2F1tgb87cq!3m5!1s0x89b7b7bcdecbb1df:0x715969d86d0b76bf!8m2!3d38.8976763!4d-77.0365298!16zL20vMDgxc3E?hl=en&authuser=0',
+      icon:<MapIcon fontSize={contactIconsSize} color={iconColor}/>,
     },
     hours:{
       hours:'9-5 Monday Through Friday',
-      icon:<AccessTimeFilledIcon fontSize='large'/>,
+      icon:<AccessTimeFilledIcon fontSize={contactIconsSize} color={iconColor}/>,
     }
   },
   signature:{
